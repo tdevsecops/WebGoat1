312db78704484d789e7a92d60c5a1ba7bfcdba5c
tdevsecops <133196252+tdevsecops@users.noreply.github.com>
2024-10-23 14:43:38 +0530

    reomove

--- WebGoat/src/PetClinicApplication.java
+++ WebGoat/src/PetClinicApplication.java
@@ -24,18 +24,3 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- *
- */
-@SpringBootApplication
-@ImportRuntimeHints(PetClinicRuntimeHints.class)
-public class PetClinicApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
-	}
-
-}
