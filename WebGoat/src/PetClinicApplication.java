5933466633dd946a653ff0e4e9a9a010708c234d
tdevsecops <133196252+tdevsecops@users.noreply.github.com>
2024-10-23 15:10:52 +0530

    changes done

--- WebGoat/src/PetClinicApplication.java
+++ WebGoat/src/PetClinicApplication.java
@@ -14,13 +14,4 @@
  * limitations under the License.
  */
 
-package org.springframework.samples.petclinic;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
